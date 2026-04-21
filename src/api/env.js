7b2c0dd00add7714a8206fb378c725040fd865c3
kevin raulino <kevinamraulino@gmail.com>
2026-04-21 15:45:52 -0300

    Correção na api para conectar com o banco

--- src/api/env.js
+++ src/api/env.js
@@ -2,4 +2,4 @@
 // Futuramente substituir pelo valor de process.env.REACT_APP_API_URL
 // Exemplo: const API_BASE_URL = process.env.REACT_APP_API_URL;
 
-export const API_BASE_URL = "http://localhost:8080";
+export const API_BASE_URL = "http://localhost:8080/api";
